b9a3371e17aa89d739b64879a7a5d2ec0146d465
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-07 20:02:26 +0800

    sync daily branch 1.1.2

--- doc/build/lib/font-family/cmd.js
+++
@@ -1,1 +0,0 @@
-KISSY.add('editor-plugins/lib/font-family/cmd',["../font/cmd"],function(S ,require, exports, module) {var e=require("../font/cmd"),t={element:"span",styles:{"font-family":"#(value)"},overrides:[{element:"font",attributes:{face:null}}]};module.exports={init:function(n){e.addSelectCmd(n,"fontFamily",t)}};});
