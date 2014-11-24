1a55098bc5e6b5b0b48c4590864d91b1b4147c2b
弘树 <tiehang.lth@alibaba-inc.com>
2014-11-25 02:52:20 +0800

    sync daily branch 1.1.7

--- doc/build/lib/justify-left/cmd.js
+++
@@ -1,1 +0,0 @@
-KISSY.add('editor-plugins/lib/justify-left/cmd',["../justify-cmd"],function(S ,require, exports, module) {var t=require("../justify-cmd");module.exports={init:function(i){t.addCommand(i,"justifyLeft","left")}};});
